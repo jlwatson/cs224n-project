21486bfc479f5b7679bc16e3f7a339662f0b4a91
Varun Ramesh <varunramesh@live.com>
2018-02-28 07:06:36 -0800

    Traverse pages.

--- data/satoshi/download-bitcoin-talk.py
+++ data/satoshi/download-bitcoin-talk.py
@@ -1,7 +1,12 @@
 import html2text
-
+import argparse
+import time
 from requests_html import HTMLSession
 from slugify import slugify
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url', help='The Profile url.')
+args = parser.parse_args()
 
 h = html2text.HTML2Text()
 h.ignore_links = True
@@ -9,13 +14,22 @@ h.ignore_images = True
 
 session = HTMLSession()
 
-r = session.get('https://bitcointalk.org/index.php?action=profile;u=224;sa=showPosts;start=0')
-td = r.html.find('#bodyarea td', first=True)
-posts = td.find('table[cellpadding="0"]')
-print(len(posts))
+cursor = 0
+while True:
+    url = args.url + ';sa=showPosts;start=' + str(cursor)
+    print("Crawling", url)
+    r = session.get(url)
+    td = r.html.find('#bodyarea td', first=True)
+    posts = td.find('table[cellpadding="0"]')
 
-for post in posts:
-    url = post.find('.middletext a')[-1].attrs['href']
-    content = post.find('.post', first=True).html
-    with open(slugify(url) + ".txt", "wb") as f:
-        f.write(h.handle(content).encode('utf8'))
+    for post in posts:
+        url = post.find('.middletext a')[-1].attrs['href']
+        content = post.find('.post', first=True).html
+        with open(slugify(url) + ".txt", "wb") as f:
+            f.write(h.handle(content).encode('utf8'))
+
+    cursor += len(posts)
+    
+    time.sleep(5)
+    if len(posts) < 20:
+        break
