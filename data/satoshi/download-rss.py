0a9fe5dd43028b45175906a172c5a93400073240
Varun Ramesh <varunramesh@live.com>
2018-03-14 03:11:17 -0700

    RSS goes to next pages.

--- data/satoshi/download-rss.py
+++ data/satoshi/download-rss.py
@@ -12,8 +12,19 @@ h = html2text.HTML2Text()
 h.ignore_links = True
 h.ignore_images = True
 
-feed = parse(args.url)
+url = args.url
+while True:
+    print ("Parsing", url, "...")
+    feed = parse(url)
+    for item in feed["items"]:
+        with open(slugify(item["link"]) + ".txt", "wb") as f:
+            f.write(h.handle(item["summary"]).encode('utf8'))
 
-for item in feed["items"]:
-    with open(slugify(item["link"]) + ".txt", "wb") as f:
-        f.write(h.handle(item["summary"]).encode('utf8'))
+    if 'feed' in feed and 'links' in feed['feed']:
+        next_link = [link['href'] for link in feed['feed']['links'] if link['rel'] == 'next']
+        if len(next_link) > 0:
+            url = next_link[0]
+        else:
+            break
+    else:
+        break
