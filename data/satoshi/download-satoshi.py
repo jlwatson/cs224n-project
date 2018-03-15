10e6893b0a69b06d3638f9bbe2b1dbc211593fe9
Varun Ramesh <varunramesh@live.com>
2018-03-15 02:21:48 -0700

    Cleanup the satoshi dataset.

--- data/satoshi/download-satoshi.py
+++ data/satoshi/download-satoshi.py
@@ -1,17 +1,39 @@
 import requests
 import html2text
 
-h = html2text.HTML2Text()
-h.ignore_links = True
-h.ignore_images = True
+from itertools import filterfalse
+from pyquery import PyQuery as pq
 
-emails = requests.get('https://raw.githubusercontent.com/NakamotoInstitute/nakamotoinstitute.org/master/data/satoshiemails.json').json()
+def process_email(email):
+    lines = email.splitlines()
 
-for i, email in enumerate(emails['emails']):
-    with open("email-%d.txt" % i, "wb") as f:
-        f.write(email['Text'].encode('utf8'))
+    lines = filterfalse(lambda x: x == 'Satoshi Nakamoto' or x == 'Satoshi', lines)
+    lines = filterfalse(lambda x: x == '---------------------------------------------------------------------', lines)
+    lines = filterfalse(lambda x: x == 'The Cryptography Mailing List', lines)
+    lines = filterfalse(lambda x: x.startswith('Unsubscribe by sending "unsubscribe cryptography" to'), lines)
 
-posts = requests.get('https://raw.githubusercontent.com/NakamotoInstitute/nakamotoinstitute.org/master/data/satoshiposts.json').json()
-for i, post in enumerate(posts['posts']):
-    with open("post-%d.txt" % i, "wb") as f:
-        f.write(h.handle(post['post']).encode('utf8'))
+    lines = filterfalse(lambda x: x.startswith('>'), lines)
+    lines = filterfalse(lambda x: x.endswith('wrote:'), lines)
+    return '\n'.join(lines)
+
+def process_post(post_html):
+    post = pq(post_html)
+    post.remove('div.quoteheader')
+    print(post.html(method='html'))
+    return post.html(method='html')
+
+if __name__ == "__main__":
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    h.ignore_images = True
+
+    emails = requests.get('https://raw.githubusercontent.com/NakamotoInstitute/nakamotoinstitute.org/master/data/satoshiemails.json').json()
+
+    for i, email in enumerate(emails['emails']):
+        with open("email-%d.txt" % i, "wb") as f:
+            f.write(process_email(email['Text']).encode('utf8'))
+
+    posts = requests.get('https://raw.githubusercontent.com/NakamotoInstitute/nakamotoinstitute.org/master/data/satoshiposts.json').json()
+    for i, post in enumerate(posts['posts']):
+        with open("post-%d.txt" % i, "wb") as f:
+            f.write(h.handle(process_post(post['post'])).encode('utf8'))
