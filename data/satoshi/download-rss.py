0eb002ce450c2d9bd16a65548339bb012e2d5967
Varun Ramesh <varunramesh@live.com>
2018-03-15 01:53:59 -0700

    Get blog comments.

--- data/satoshi/download-rss.py
+++ data/satoshi/download-rss.py
@@ -4,27 +4,54 @@ import argparse
 from feedparser import parse
 from slugify import slugify
 
-parser = argparse.ArgumentParser()
-parser.add_argument('url', help='The RSS url.')
-args = parser.parse_args()
+def download_author_comments(replies_url, author_names):
+    url = replies_url
+    while True:
+        print ("Parsing comments ", url, "...")
+        feed = parse(url)
+        for item in feed["items"]:
+            if item['author_detail']['name'] in author_names:
+                with open(slugify(item["link"]) + ".txt", "wb") as f:
+                    f.write(h.handle(item["summary"]).encode('utf8'))
 
-h = html2text.HTML2Text()
-h.ignore_links = True
-h.ignore_images = True
-
-url = args.url
-while True:
-    print ("Parsing", url, "...")
-    feed = parse(url)
-    for item in feed["items"]:
-        with open(slugify(item["link"]) + ".txt", "wb") as f:
-            f.write(h.handle(item["summary"]).encode('utf8'))
-
-    if 'feed' in feed and 'links' in feed['feed']:
-        next_link = [link['href'] for link in feed['feed']['links'] if link['rel'] == 'next']
-        if len(next_link) > 0:
-            url = next_link[0]
+        if 'feed' in feed and 'links' in feed['feed']:
+            next_link = [link['href'] for link in feed['feed']['links'] if link['rel'] == 'next']
+            if len(next_link) > 0:
+                url = next_link[0]
+            else:
+                break
         else:
             break
-    else:
-        break
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='The RSS url.')
+    parser.add_argument('--authornames', nargs='+',
+                    help='Names that the author uses in the comments of their blog.')
+    args = parser.parse_args()
+
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    h.ignore_images = True
+
+    url = args.url
+    while True:
+        print ("Parsing", url, "...")
+        feed = parse(url)
+
+        for item in feed["items"]:
+            # with open(slugify(item["link"]) + ".txt", "wb") as f:
+            #     f.write(h.handle(item["summary"]).encode('utf8'))
+            if args.authornames and 'links' in item:
+                replies_link = [link['href'] for link in item['links'] if link['rel'] == 'replies']
+                if len(replies_link) > 0:
+                    download_author_comments(replies_link[0], args.authornames)
+
+        if 'feed' in feed and 'links' in feed['feed']:
+            next_link = [link['href'] for link in feed['feed']['links'] if link['rel'] == 'next']
+            if len(next_link) > 0:
+                url = next_link[0]
+            else:
+                break
+        else:
+            break
