463593ea671d0f5286fff260c9dc50c034382586
Varun Ramesh <varunramesh@live.com>
2018-03-18 20:25:31 -0700

    Multiplicative attention

--- attention_layer.py
+++ attention_layer.py
@@ -5,12 +5,19 @@ from keras.engine.topology import Layer
 from keras.layers import Wrapper, Recurrent
 
 class Attention(Layer):
-    def __init__(self, direction = "forward", **kwargs):
+    def __init__(self, direction = "forward", variant="dot-product", **kwargs):
         self.direction = direction
+        self.variant = variant
         super(Attention, self).__init__(**kwargs)
 
     def build(self, input_shape):
         assert len(input_shape) == 3
+        samples, time_steps, hidden_dim = input_shape[0], input_shape[1], input_shape[2]
+
+        if self.variant == "multiplicative":
+            self.w = self.add_weight(shape=(hidden_dim, hidden_dim),
+                initializer='uniform', name='kernel')
+
         super(Attention, self).build(input_shape)
 
     def call(self, x):
@@ -36,6 +43,9 @@ class Attention(Layer):
                 backward_states[:, 0, :]])
         else:
             raise ArgumentTypeError("Invalid direction %s" % self.direction)
+
+        if self.variant == "multiplicative":
+            prev_hidden_states = K.dot(prev_hidden_states, self.w)
 
         scores = K.sum(prev_hidden_states * K.expand_dims(final_hidden_state, axis=1), axis=2)
         weights = K.softmax(scores)
